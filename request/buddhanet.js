6dc508b43882dbb7cd6d1c5f6db5cbd83e6f2900
John Tribolet <john@tribolet.info>
2016-08-06 22:11:58 -0400

    BuddaNet - Process categories and clean up PDF urls.

--- request/buddhanet.js
+++ request/buddhanet.js
@@ -1,6 +1,11 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs      = require('fs');
+var async   = require('async');
+
+
+var baseUrl    = 'http://www.buddhanet.net/';
+var ebooksHome = baseUrl + 'ebooks.htm';
 
 
 function download(url, dest, cb) {
@@ -12,13 +17,44 @@ function download(url, dest, cb) {
     });
 }
 
-var ebooksHome = 'http://www.buddhanet.net/ebooks.htm';
-var cats = [];
+function processCategories(cats) {
+    async.each(cats, function(category, cbFinished) {
+        request(baseUrl + category.link, function(err, res, body) {
+            console.log('----------------');
+            console.log(category.name);
+            var $ = cheerio.load(body);
+
+            // get download urls
+            var links = [];
+            $('#wlinks table table a').each(function(i, elem) {
+                var link = $(this).attr('href');
+
+                if ( typeof link !== 'undefined' && link.includes('pdf') ) {
+                    if (! link.includes('buddhanet.net')) {
+                        link = baseUrl + link;
+                    }
+
+                    links.push(link);
+                }
+            });
+
+            console.log(links);
+            cbFinished();
+        })
+    });
+}
+
+function downloadLinks(category, links) {
+    if (!fs.existsSync(category)){
+        fs.mkdirSync(category);
+    }
+}
 
 // get category links
 request(ebooksHome, function(err, res, body) {
 
-    var $ = cheerio.load(body);
+    var cats      = [];
+    var $         = cheerio.load(body);
     var menuLinks = $('.linksMenu a');
 
     menuLinks.each(function(i, elem) {
@@ -27,8 +63,11 @@ request(ebooksHome, function(err, res, body) {
             link: $(this).attr('href')
         };
 
-        cats.push(item);
+        if ('Buddhanet eBooks Home' !== item.name) {
+            cats.push(item);
+        }
     });
 
-    // console.log(cats);
+    console.log(cats);
+    processCategories(cats);
 });
