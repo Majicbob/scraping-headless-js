76a10498528044d8d7811ef816674afddd9a428c
John Tribolet <john@tribolet.info>
2016-08-08 11:11:03 -0400

    BuddhaNet - Change varibles that aren't reassigned to const.

--- request/buddhanet.js
+++ request/buddhanet.js
@@ -7,18 +7,18 @@
 
 'use strict';
 
-let request    = require('request');
-let cheerio    = require('cheerio');
-let fs         = require('fs');
-let async      = require('async');
+const request    = require('request');
+const cheerio    = require('cheerio');
+const fs         = require('fs');
+const async      = require('async');
 
 
-let baseUrl    = 'http://www.buddhanet.net/';
-let ebooksHome = baseUrl + 'ebooks.htm';
+const baseUrl    = 'http://www.buddhanet.net/';
+const ebooksHome = baseUrl + 'ebooks.htm';
 
 // download and save a file
 function download(url, dest, cb) {
-    let file = fs.createWriteStream(dest);
+    const file = fs.createWriteStream(dest);
     request(url).pipe(file);
     file.on('finish', function() {
         console.log('Download Finished: ' + dest);
@@ -34,9 +34,9 @@ function downloadLinks(category, links) {
 
     async.each(links, function(link) {
         // get filename from url
-        let startOfName = link.lastIndexOf('/') + 1;
-        let filename    = link.substring(startOfName);
-        let destPath    = category + '/' + filename;
+        const startOfName = link.lastIndexOf('/') + 1;
+        const filename    = link.substring(startOfName);
+        const destPath    = category + '/' + filename;
 
         // download if file doesn't exist
         fs.stat(destPath, (err) => {
@@ -50,7 +50,7 @@ function downloadLinks(category, links) {
 // load a category page, parse and clean up all the ebook download links
 function processCategories(cats) {
     async.each(cats, function(category, cbFinished) {
-        let categoryUrl = baseUrl + category.link;
+        const categoryUrl = baseUrl + category.link;
         request(categoryUrl, function(err, resp, body) {
             if (err) {
                 console.error('Error loading category page ' + categoryUrl + '\n ' + err);
@@ -58,8 +58,8 @@ function processCategories(cats) {
             }
 
             // get download urls
-            let $     = cheerio.load(body);
-            let links = [];
+            const $     = cheerio.load(body);
+            const links = [];
 
             $('#wlinks table table a').each(function(i, elem) {
                 let link = $(elem).attr('href');
@@ -90,12 +90,12 @@ request(ebooksHome, function(err, resp, body) {
         return;
     }
 
-    let cats      = [];
-    let $         = cheerio.load(body);
-    let menuLinks = $('.linksMenu a');
+    const cats      = [];
+    const $         = cheerio.load(body);
+    const menuLinks = $('.linksMenu a');
 
     menuLinks.each(function(i, elem) {
-        let item = {
+        const item = {
             name: $(elem).text(),
             link: $(elem).attr('href')
         };
