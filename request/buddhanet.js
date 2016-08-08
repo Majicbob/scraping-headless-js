e165693117b5c5bc4a4601fd50ae1e0051595851
John Tribolet <john@tribolet.info>
2016-08-08 04:56:11 -0400

    BuddhaNet - Fix lint issues.

--- request/buddhanet.js
+++ request/buddhanet.js
@@ -7,48 +7,66 @@
 
 'use strict';
 
-let request = require('request');
-let cheerio = require('cheerio');
-let fs      = require('fs');
-let async   = require('async');
+let request    = require('request');
+let cheerio    = require('cheerio');
+let fs         = require('fs');
+let async      = require('async');
 
 
 let baseUrl    = 'http://www.buddhanet.net/';
 let ebooksHome = baseUrl + 'ebooks.htm';
 
-
 // download and save a file
 function download(url, dest, cb) {
     let file = fs.createWriteStream(dest);
     request(url).pipe(file);
-    file.on('finish', function () {
-        console.log('Downloaded ' + dest);
+    file.on('finish', function() {
+        console.log('Download Finished: ' + dest);
         file.close(cb);
+    });
+}
+
+// create directory for the category and save all files to it without overwrite
+function downloadLinks(category, links) {
+    if (!fs.existsSync(category)){
+        fs.mkdirSync(category);
+    }
+
+    async.each(links, function(link) {
+        // get filename from url
+        let startOfName = link.lastIndexOf('/') + 1;
+        let filename    = link.substring(startOfName);
+        let destPath    = category + '/' + filename;
+
+        // download if file doesn't exist
+        fs.stat(destPath, (err) => {
+            if (null !== err && 'ENOENT' === err.code) {
+                download(link, destPath);
+            }
+        });
     });
 }
 
 // load a category page, parse and clean up all the ebook download links
 function processCategories(cats) {
-    async.each(cats, function (category, cbFinished) {
+    async.each(cats, function(category, cbFinished) {
         let categoryUrl = baseUrl + category.link;
-        request(categoryUrl, function (err, res, body) {
+        request(categoryUrl, function(err, resp, body) {
             if (err) {
-                console.log('Error loading category page ' + categoryUrl + '\n ' + err);
+                console.error('Error loading category page ' + categoryUrl + '\n ' + err);
                 return;
             }
 
-            console.log('----------------');
-            console.log(category.name);
-            let $ = cheerio.load(body);
-
             // get download urls
+            let $     = cheerio.load(body);
             let links = [];
-            $('#wlinks table table a').each(function (i, elem) {
-                let link = $(this).attr('href');
+
+            $('#wlinks table table a').each(function(i, elem) {
+                let link = $(elem).attr('href');
 
                 // only get pdf or zip files
-                if ( typeof link !== 'undefined'
-                     && ( link.includes('.pdf') | link.includes('.zip') )
+                if ( 'undefined' !== typeof link
+                     && ( link.includes('.pdf') || link.includes('.zip') )
                 ) {
                     // add base url if its a relative link
                     if (! link.includes('buddhanet.net')) {
@@ -59,37 +77,16 @@ function processCategories(cats) {
                 }
             });
 
-            console.log(links);
             downloadLinks(category.name, links);
             cbFinished();
-        })
-    });
-}
-
-// create directory for the category and save all files to it
-function downloadLinks(category, links) {
-    console.log('Download Category: ' + category);
-    if (!fs.existsSync(category)){
-        fs.mkdirSync(category);
-    }
-
-    async.each(links, function (link, cbFinished) {
-        // get filename from url
-        let startOfName = link.lastIndexOf('/') + 1;
-        let filename    = link.substring(startOfName);
-        let destPath    = category + '/' + filename;
-
-        // download if file doesn't exist
-        fs.stat(destPath, (err, stat) => { if (err.code == 'ENOENT') {
-            download(link, destPath);
-        }});
+        });
     });
 }
 
 // get category links
-request(ebooksHome, function (err, res, body) {
+request(ebooksHome, function(err, resp, body) {
     if (err) {
-        console.log('Error loading main page ' + ebooksHome + '\n ' + err);
+        console.error('Error loading main page ' + ebooksHome + '\n ' + err);
         return;
     }
 
@@ -97,10 +94,10 @@ request(ebooksHome, function (err, res, body) {
     let $         = cheerio.load(body);
     let menuLinks = $('.linksMenu a');
 
-    menuLinks.each(function (i, elem) {
+    menuLinks.each(function(i, elem) {
         let item = {
-            name: $(this).text(),
-            link: $(this).attr('href')
+            name: $(elem).text(),
+            link: $(elem).attr('href')
         };
 
         if ('Buddhanet eBooks Home' !== item.name) {
@@ -108,6 +105,5 @@ request(ebooksHome, function (err, res, body) {
         }
     });
 
-    console.log(cats);
     processCategories(cats);
 });
